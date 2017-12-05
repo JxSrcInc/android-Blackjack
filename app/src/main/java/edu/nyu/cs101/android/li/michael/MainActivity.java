80da36b9e69efe708d939ae7db0abf285f813614
johnligithub <johnli132@gmail.com>
2017-12-05 18:03:15 -0500

    add custom icon

--- app/src/main/java/edu/nyu/cs101/android/li/michael/MainActivity.java
+++ app/src/main/java/edu/nyu/cs101/android/li/michael/MainActivity.java
@@ -17,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
 Intent intent = new Intent(this, DisplayMessageActivity.class);
-EditText editText = (EditText) findViewById(R.id.editText);
-String message = editText.getText().toString();
-intent.putExtra(EXTRA_MESSAGE, message);
+        TextView editText = (TextView) findViewById(R.id.titleView);
+//String message = editText.getText().toString();
+//intent.putExtra(EXTRA_MESSAGE, message);
 startActivity(intent);
 
     }
